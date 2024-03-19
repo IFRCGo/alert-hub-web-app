427e74e59adf0c73b4d824c7112a0b8a4ef0da9d
barshathakuri <barshathakuri708@gmail.com>
2024-03-19 11:39:57 +0545

    Add graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App/index.tsx';
+import App from './App';
 
 const webappRootId = 'webapp-root';
 
