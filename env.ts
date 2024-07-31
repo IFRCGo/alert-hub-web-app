918f8d2abc235f7b4834fff5db74117a7adf8ef4
barshathakuri <barshathakuri708@gmail.com>
2024-07-31 16:47:28 +0545

    Integrate google analytics

--- env.ts
+++ env.ts
@@ -5,4 +5,5 @@ export default defineConfig({
     APP_TITLE: Schema.string.optional(),
     APP_MAPBOX_ACCESS_TOKEN: Schema.string(),
     APP_GRAPHQL_ENDPOINT: Schema.string.optional(),
+    APP_GOOGLE_ANALYTICS_ID: Schema.string.optional(),
 })
